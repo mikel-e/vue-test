dd4cfa43b6af18372fbf83afa8ad33fcef68bb2a
mikel-e <mikel-e@qq.com>
2025-04-06 22:03:32 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import process from 'node:process';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -15,4 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  base: process.env.NODE_ENV === 'production' ? '/vue-test/' : '/',
 })
